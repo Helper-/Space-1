b1b9908b1eb6781db448f68ecf6fce6e303b97ec
Timothy Halim <thalim@ucsd.edu>
2016-03-11 17:59:00 -0800

    still working on modal messages for uploading

--- routes/webapp/business/customizetheme.js
+++ routes/webapp/business/customizetheme.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var auth = require('../../../lib/auth');
+var style = require('../../../lib/auth');
 
  exports.get = function (req, res, next) {
 
@@ -22,4 +23,71 @@ var auth = require('../../../lib/auth');
              bg: '/images/bg/full/' + business.style.bg
          });
      });
+ };
+
+ exports.post = function(req, res, next){
+
+     var db = req.db;
+     var businesses = db.get('businesses');
+     var businessID = req.user[0].business;
+
+     if(req.files.userLogo){
+
+         businesses.findById(businessID,
+             function (err, results){
+
+                 if(err){
+                     return next(err);
+                 }
+
+                 fs.unlink('public'+results.logo);
+             }
+         );
+
+         businesses.updateById(businessID, {
+                 $set: {
+                     logo: '/images/uploads/' + req.files.userLogo.name
+                 }
+             },{
+                 upsert: true
+             }, function (err){
+                 if (err) {
+                     return next(err);
+                 }
+
+                 res.render('business/customizetheme',{
+                     success:'Succesfully uploaded file: '+req.files.userLogo.originalname,
+                     logo:'/images/uploads/'+req.files.userLogo.name
+                 });
+
+             }
+
+         );
+     }
+     else{
+
+         businesses.findById(businessID,
+             function (err, results){
+                 if(err){
+                     return next(err);
+                 }
+
+                 if(results.logo){
+
+                     res.render('business/customizetheme',{
+                         title:'Upload Logo',
+                         logo:results.logo,
+                         error:'Please select a valid image(png,jpg) file to upload.'
+                     });
+                 }
+                 else{
+                     res.render('business/customizetheme',{
+                         title:'Upload Logo',
+                         error:'Please select a valid image(png,jpg) file to upload.'
+                     });
+                 }
+             }
+         );
+     }
+
  };
