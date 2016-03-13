bcadef573b478e9c33c176f8aba6ebb45f20c542
Chris Stringham <C.J.Stringham@gmail.com>
2016-03-12 19:43:41 -0800

    uploads of stuffs

--- routes/webapp/business/customizetheme.js
+++ routes/webapp/business/customizetheme.js
@@ -2,64 +2,15 @@ var fs = require('fs');
 var auth = require('../../../lib/auth');
 var style = require('../../../lib/auth');
 
-<<<<<<< HEAD
 exports.get = function (req, res, next) {
 
    var businessDB = req.db.get('businesses');
-=======
- exports.get = function (req, res, next) {
-   var db = req.db;
-   var businesses = db.get('businesses');
->>>>>>> d50b073d1d2640d7fc35c41c775bf0d31bd404a4
    var businessID = req.user[0].business;
      //Get the logo and default BG for the business of the currently logged in user
-     businesses.findById(businessID, function (err, business) {
-         if (err) { return next(err); }
-         if (!business) { return next(new Error('Business not found for user: ' + req.user)); }
 
-<<<<<<< HEAD
-   //Get the logo for the business of the currently logged in user
    businessDB.findById(req.user[0].business, function (err, business) {
      if (err) {
        return next(err);
-=======
-         //Show images on the screen
-         res.render('business/customizetheme', {
-             message: req.flash('permission'),
-             logo: business.logo,
-             bg: '/images/bg/full/' + business.style.bg
-         });
-     }); //Closes findByID
- };
-
- exports.post = function(req, res, next){
-     var db = req.db;
-     var businesses = db.get('businesses');
-     var businessID = req.user[0].business;
-
-     if(req.files.userLogo){
-         businesses.findById(businessID, function (err, results) {
-                 if(err) { return next(err); }
-                 fs.unlink('public'+results.logo);
-             }
-         ); //Closes findByID
-
-         businesses.updateById(businessID, {
-                 $set: {
-                     logo: '/images/uploads/' + req.files.userLogo.name,
-                 }
-             },
-              { upsert: true },
-              function (err) {
-                 if (err) { return next(err); }
-
-                 res.render('business/customizetheme',{
-                     success:'Succesfully uploaded file: '+req.files.userLogo.originalname,
-                     logo:'/images/uploads/'+req.files.userLogo.name
-                 });
-             }
-         ); //Closes updateByID
->>>>>>> d50b073d1d2640d7fc35c41c775bf0d31bd404a4
      }
 
      res.render('business/customizetheme', {
