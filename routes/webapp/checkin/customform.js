2c1f02498eded1a4b3b99f17c7b8d84db4d45ff4
Michael Chang <chang.dongsung@gmail.com>
2016-03-15 00:54:51 -0700

    Forms shoudl now show up in customform portion of the checkin process. It looks ugly and that needs to be fixed. Also submitting the form will do nothing for now, kind of bypasses that as we need to figure out how to do the post method for custom forms. But it semi-works? at least it's somewhat functional

--- routes/webapp/checkin/customform.js
+++ routes/webapp/checkin/customform.js
@@ -4,115 +4,141 @@ var style = require('./../../../lib/style.js');
 var sms = require('./../../../lib/sms.js');
 
 //Custom Form
-function makeDropdown(options, name, body) {
-    var s = '<select class="form-control" name="'+name+'" id="'+name+'">';
-    _.each(options, function (option) {
-        s += '<option value="'+option+'" ' + (body[name] === option ? 'selected' : '') + '>'+option+'</option> ';
-    });
-    s+= '</select>';
-    return s;
-}
-
-function makeTextfield(name, body) {
-    return'<input type="text" class="form-control form-width-custom" name="'+name+'" id="' + name + '"value="' + (body[name] || '') + '">';
-}
-
-function makeFormGroup(field, index, body) {
-    var name = '_' + index;
-
-    var s = '<div class="form-group">';
-    s += '<label for="' + name + '" class="col-md-4 control-label">' + field.label + '</label>';
-
-    s += '<div class="col-md-8">';
-    if (field.type === 'textfield') {
-        s += makeTextfield(name, body);
-    } else if (field.type === 'dropdown') {
-        s += makeDropdown(field.options, name, body);
-    }
-    s += '</div>';
-
-    s += '</div>';
-    return s;
-}
-
-function makeSubmitButton() {
-    var s = '<div class="form-group">';
-    s += '<div>';
-    s += '<button type="submit" class="btn btn-lg btn-block custom-button custom-center">Submit</button>';
-    s += '</div>';
-    s += '</div>';
-
-    return s;
-}
-/**
- *
- *
- * @param db The MongoDB object
- * @param businessId The id for the business
- * @param body The form body (used to prefill form)
- * @param fn Callback function `fn(formHtml)`
- */
-function makeForm(db, businessId, body, fn) {
-    var businesses = db.get('businesses');
-    var forms = db.get('forms');
-
-    //Find the business and get their form
-    businesses.findById(businessId, function (err, business) {
-        if (err) {
-            return fn(err);
-        }
-        if (!business) {
-            return fn(new Error('Business not found: ' + businessId));
-        }
-        forms.findOne({business: business._id}, function (err, form) {
-            if (err) {
-                return fn(err);
-            }
-            if (!form) {
-                return fn(new Error('Form not found for business: ' + businessId));
-            }
-
-            var formHtml = '<form class="form-horizontal" method="post" action="customform" enctype="application/x-www-form-urlencoded">';
-
-            _.each(form.fields, function (field, index) {
-                formHtml += makeFormGroup(field, index, body);
-            });
-
-            formHtml += makeSubmitButton();
-
-            formHtml += '</form>';
-
-            fn(null, formHtml);
-        });
-    });
-}
+//function makeDropdown(options, name, body) {
+//    var s = '<select class="form-control" name="'+name+'" id="'+name+'">';
+//    _.each(options, function (option) {
+//        s += '<option value="'+option+'" ' + (body[name] === option ? 'selected' : '') + '>'+option+'</option> ';
+//    });
+//    s+= '</select>';
+//    return s;
+//}
+//
+//function makeTextfield(name, body) {
+//    return'<input type="text" class="form-control form-width-custom" name="'+name+'" id="' + name + '"value="' + (body[name] || '') + '">';
+//}
+//
+//function makeFormGroup(field, index, body) {
+//    var name = '_' + index;
+//
+//    var s = '<div class="form-group">';
+//    s += '<label for="' + name + '" class="col-md-4 control-label">' + field.label + '</label>';
+//
+//    s += '<div class="col-md-8">';
+//    if (field.type === 'textfield') {
+//        s += makeTextfield(name, body);
+//    } else if (field.type === 'dropdown') {
+//        s += makeDropdown(field.options, name, body);
+//    }
+//    s += '</div>';
+//
+//    s += '</div>';
+//    return s;
+//}
+//
+//function makeSubmitButton() {
+//    var s = '<div class="form-group">';
+//    s += '<div>';
+//    s += '<button type="submit" class="btn btn-lg btn-block custom-button custom-center">Submit</button>';
+//    s += '</div>';
+//    s += '</div>';
+//
+//    return s;
+//}
+///**
+// *
+// *
+// * @param db The MongoDB object
+// * @param businessId The id for the business
+// * @param body The form body (used to prefill form)
+// * @param fn Callback function `fn(formHtml)`
+// */
+//function makeForm(db, businessId, body, fn) {
+//    var businesses = db.get('businesses');
+//    var forms = db.get('forms');
+//
+//    //Find the business and get their form
+//    businesses.findById(businessId, function (err, business) {
+//        if (err) {
+//            return fn(err);
+//        }
+//        if (!business) {
+//            return fn(new Error('Business not found: ' + businessId));
+//        }
+//        forms.findOne({business: business._id}, function (err, form) {
+//            if (err) {
+//                return fn(err);
+//            }
+//            if (!form) {
+//                return fn(new Error('Form not found for business: ' + businessId));
+//            }
+//
+//            var formHtml = '<form class="form-horizontal" method="post" action="customform" enctype="application/x-www-form-urlencoded">';
+//
+//            _.each(form.fields, function (field, index) {
+//                formHtml += makeFormGroup(field, index, body);
+//            });
+//
+//            formHtml += makeSubmitButton();
+//
+//            formHtml += '</form>';
+//
+//            fn(null, formHtml);
+//        });
+//    });
+//}
 
 exports.get = function (req, res, next) {
     var business = req.session.business;
-    makeForm(req.db, req.params.id, {}, function (err, formHtml) {
+    //makeForm(req.db, req.params.id, {}, function (err, formHtml) {
+    //    if (err) {
+    //        return next(err);
+    //    }
+    //    res.render('checkin/customform', {
+    //        formHtml: formHtml,
+    //        companyName: business.companyName,
+    //        bg: business.bg,
+    //        logo: business.logo,
+    //        buttonBg: style.rgbObjectToCSS(business.buttonBg),
+    //        buttonText: style.rgbObjectToCSS(business.buttonText),
+    //        containerText: style.rgbObjectToCSS(business.containerText),
+    //        containerBg: style.rgbObjectToCSS(business.containerBg)
+    //    });
+    //});
+    var forms = req.db.get('forms');
+    var businessID = req.user[0].business;
+    forms.findOne({business: businessID}, function (err, form_data) {
         if (err) {
             return next(err);
         }
-        res.render('checkin/customform', {
-            formHtml: formHtml,
-            companyName: business.companyName,
-            bg: business.style.bg,
-            logo: business.logo,
-            buttonBg: style.rgbObjectToCSS(business.style.buttonBg),
-            buttonText: style.rgbObjectToCSS(business.style.buttonText),
-            containerText: style.rgbObjectToCSS(business.style.containerText),
-            containerBg: style.rgbObjectToCSS(business.style.containerBg)
-        });
+        var formHtml = form_data.data;
+        if(form_data !== null) {
+            res.render('checkin/customform', {
+                formHtml: formHtml,
+                companyName: business.companyName,
+                bg: business.bg,
+                logo: business.logo,
+                buttonBg: style.rgbObjectToCSS(business.buttonBg),
+                buttonText: style.rgbObjectToCSS(business.buttonText),
+                containerText: style.rgbObjectToCSS(business.containerText),
+                containerBg: style.rgbObjectToCSS(business.containerBg)
+            });
+        }
+
+        else {
+            return fn(new Error('Business not found: ' + businessId));
+        }
+
     });
 };
 
 exports.post = function (req, res, next) {
     var db = req.db;
-    var forms = db.get('forms');
+    var forms = req.db.get('forms');
+    var businessID = req.user[0].business;
 
     var business = req.session.business;
 
-    forms.findOne({business: ObjectID(business._id)}, function (err, form) {
+    forms.findOne({business: businessID}, function (err, form) {
         if (err) {
             return next(err);
         }
@@ -128,18 +154,29 @@ exports.post = function (req, res, next) {
         });
 
         if (!valid) {
-            makeForm(db, req.params.id, req.body, function (formHtml) {
-                res.render('checkin/customform', {
-                    formHtml: formHtml,
-                    formError: 'You are missing required fields.',
-                    companyName: business.companyName,
-                    bg: business.style.bg,
-                    logo: business.logo,
-                    buttonBg: style.rgbObjectToCSS(business.style.buttonBg),
-                    buttonText: style.rgbObjectToCSS(business.style.buttonText),
-                    containerText: style.rgbObjectToCSS(business.style.containerText),
-                    containerBg: style.rgbObjectToCSS(business.style.containerBg)
-                });
+            //makeForm(db, req.params.id, req.body, function (formHtml) {
+            //    res.render('checkin/customform', {
+            //        formHtml: formHtml,
+            //        formError: 'You are missing required fields.',
+            //        companyName: business.companyName,
+            //        bg: business.bg,
+            //        logo: business.logo,
+            //        buttonBg: style.rgbObjectToCSS(business.buttonBg),
+            //        buttonText: style.rgbObjectToCSS(business.buttonText),
+            //        containerText: style.rgbObjectToCSS(business.containerText),
+            //        containerBg: style.rgbObjectToCSS(business.containerBg)
+            //    });
+            //});
+            res.render('checkin/customform', {
+                formHtml: form.data,
+                formError: 'You are missing required fields.',
+                companyName: business.companyName,
+                bg: business.bg,
+                logo: business.logo,
+                buttonBg: style.rgbObjectToCSS(business.buttonBg),
+                buttonText: style.rgbObjectToCSS(business.buttonText),
+                containerText: style.rgbObjectToCSS(business.containerText),
+                containerBg: style.rgbObjectToCSS(business.containerBg)
             });
         } else { //Form is valid, let's put it into the DB
             var formResponses = db.get('formResponses');
@@ -184,13 +221,13 @@ exports.post = function (req, res, next) {
                             return next(new Error('Employee not found'));
                         }
 
-                        sms.sendText(employee.phone, 'Your patient ' + appt.fname + ' ' + appt.lname + ' has checked in.', function (err) {
-                            if (err) {
-                                return next(err);
-                            }
+                        //sms.sendText(employee.phone, 'Your patient ' + appt.fname + ' ' + appt.lname + ' has checked in.', function (err) {
+                        //    if (err) {
+                        //        return next(err);
+                        //    }
 
-                            res.redirect('sign');
-                        });
+                        res.redirect('sign');
+                        //});
                     });
                 });
             });
