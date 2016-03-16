fb7c1b71354a2468e4d6c6b7726a0292dac5bad7
william-k <william.kuntz@icloud.com>
2016-03-16 15:14:12 -0700

    updated the role so that only admins can enter. got rid of landing junk...

--- routes/webapp/business/admindashboard.js
+++ routes/webapp/business/admindashboard.js
@@ -5,27 +5,14 @@ exports.get = function (req, res) {
     var employeename = req.user[0].fname;
     var businessId = req.user[0].business;
     var role = req.user[0].role;
-    console.log("\ncan you see me\n");
-    //alert("here");
-    if(role === 'admin' || role === 'receptionist' || role === 'employee') {
+
+    if(role === 'admin') {
         res.render('business/admindashboard', {
             eid: employeeId,
             employeeName: employeename,
             bid: businessId,
             message: req.flash("permission")
         });
-    }
-
-    if(role === 'checkin') {
-        console.log(businessId);
-        var business = req.user[0].business;
-
-        res.redirect('/office/checkin');
-//		res.render('checkin/checkin', {
-//        companyName: business.companyName,
-//        bg: business.bg,
-//        logo: business.logo,
-//    });
     }
 
 };
