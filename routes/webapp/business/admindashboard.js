8865acedc9891e64ab8273f4a8dff573e1d382cb
william-k <william.kuntz@icloud.com>
2016-03-16 14:28:59 -0700

    admin dashboard for peter is set up and is able to be accessed. need to figure out how to access companies and such

--- routes/webapp/business/admindashboard.js
+++ routes/webapp/business/admindashboard.js
@@ -6,32 +6,26 @@ exports.get = function (req, res) {
     var businessId = req.user[0].business;
     var role = req.user[0].role;
     console.log("\ncan you see me\n");
-    alert("here");
-    res.render('business/admindashboard', {
+    //alert("here");
+    if(role === 'admin' || role === 'receptionist' || role === 'employee') {
+        res.render('business/admindashboard', {
             eid: employeeId,
             employeeName: employeename,
             bid: businessId,
             message: req.flash("permission")
         });
-//    if(role === 'admin' || role === 'receptionist' || role === 'employee') {
-//        res.render('business/admindashboard', {
-//            eid: employeeId,
-//            employeeName: employeename,
-//            bid: businessId,
-//            message: req.flash("permission")
-//        });
-//    }
-//
-//    if(role === 'checkin') {
-//        console.log(businessId);
-//        var business = req.user[0].business;
-//
-//        res.redirect('/office/checkin');
-////		res.render('checkin/checkin', {
-////        companyName: business.companyName,
-////        bg: business.bg,
-////        logo: business.logo,
-////    });
-//    }
+    }
+
+    if(role === 'checkin') {
+        console.log(businessId);
+        var business = req.user[0].business;
+
+        res.redirect('/office/checkin');
+//		res.render('checkin/checkin', {
+//        companyName: business.companyName,
+//        bg: business.bg,
+//        logo: business.logo,
+//    });
+    }
 
 };
